7d8684c4de6f338cf787930bed4870d48f1f0c31
Matt Sadowski <matt@ubuntu-2011.(none)>
2014-08-11 21:01:16 -0600

    Created base structure for accepting input and sending to generate the SVG document.

---
+++ app.js
@@ -0,0 +1,7 @@
+
+var http = require ("http");
+var serv = http.createServer (function (req, res) {
+    res.writeHead (200, { 'Content-Type': 'text/html' });
+    res.end ('<h1>So far, so good...</h1>');
+});
+serv.listen (80);
