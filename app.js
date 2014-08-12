0bdd43bc3cd673033524d277d108db556cec2d0b
Matt Sadowski <matt@ubuntu-2011.(none)>
2014-08-12 11:05:50 -0600

    Set to generate base skill lines from supplied data.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 var express = require ("express"),
     util = require ("util"),
-    xmlbuilder = require ("xmlbuilder");
+    skillmapsvg = require ("./svgbuilder.js");
 
 var app = express ();
 app.use (express.static (__dirname + "/public"));
@@ -28,149 +28,17 @@ app.route ("/*.svg").all (function (req, res) {
 
     res.type ("svg");
     console.log ("Requested SVG Image - Instructions: " + instructions);
-    var data = [];
+    var skills = [];
     try {
-        data = JSON.parse (instructions);
+        skills = JSON.parse (instructions);
     }
     catch (err) {}
 
-    var linearGradients = [];
-    var stops = [];
-    var elements = [];
-
-    // add each skill
-    if (data.length) {
-
-        stops.push ({ stop: {
-            "@offset": "0%",
-            "@stop-color": "#F00",
-            "@stop-opacity": "0"
-        }});
-        stops.push ({ stop: {
-            "@offset": "33.3%",
-            "@stop-color": "#F00",
-            "@stop-opacity": "0.8"
-        }});
-        stops.push ({ stop: {
-            "@offset": "100%",
-            "@stop-color": "#F00",
-            "@stop-opacity": "0.2"
-        }});
-
-        linearGradients.push ({ linearGradient: {
-            "@id": "gradient-1",
-            "#list": stops
-        }});
-
-        // LOOP THROUGH SKILLS HERE
-
-        // add the label
-        elements.push ({ text: {
-            "@x": "1.5in",
-            "@y": "0.5in",
-            "@class": "label",
-            "#text": "Label"
-        }});
-
-        // add the graph gradient
-        elements.push ({ rect: {
-            "@x": "1.75in",
-            "@y": "0.35in",
-            "@width": "10in",
-            "@height": "0.15in",
-            "@fill": "url(#gradient-1)"
-        }});
-
-        // add the line under the gradient
-        elements.push ({ line: {
-            "@x1": "1.75in",
-            "@y1": "0.5in",
-            "@x2": "11.75in",
-            "@y2": "0.5in",
-            "@class": "databox"
-        }});
-
-        // add a milestone marker
-        elements.push ({ circle: {
-            "@cx": "3in",
-            "@cy": "0.5in",
-            "@r": "0.04in",
-            "@fill": "black"
-        }});
-
-        // add milestone text
-        elements.push ({ text: {
-            "#list": [
-                { tspan: {
-                    "@x": "3in",
-                    "@y": "0.65in",
-                    "@class": "milestone",
-                    "#text": "This is a"
-                }},
-                { tspan: {
-                    "@x": "3in",
-                    "@y": "0.75in",
-                    "@class": "milestone",
-                    "#text": "sample milestone"
-                }}]
-        }});
-
-    }
-
-    var styleData = "\
-        text.label {\
-            text-anchor: end;\
-            font-size: 12pt;\
-        }\
-        tspan.milestone {\
-            text-anchor: middle;\
-            font-size: 8pt;\
-        }\
-        line.databox {\
-            stroke: black;\
-            stroke-width: 0.01in;\
-        }\
-    ";
-
-    // build the structure
-    var svgData = {};
-    if (elements.length) {
-        svgData = {
-            svg: {
-                "@width": "12in",
-                "@height": "3in",
-                "@version": "1.1",
-                "@xmlns": "http://www.w3.org/2000/svg",
-                defs: {
-                    style: {
-                        "@type": "text/css",
-                        "#cdata": styleData
-                    },
-                    "#list": linearGradients
-                },
-                "#list": elements
-            }
-        };
-    }
-
-    // if no elements are available, build an empty SVG
-    else {
-        svgData = {
-            svg: {
-                "@width": "12in",
-                "@height": "1in",
-                "@version": "1.1",
-                "@xmlns": "http://www.w3.org/2000/svg"
-            }
-        }
-    }
-
-    // create the SVG document
-    var svg = xmlbuilder.create (svgData, {}, {}, {headless: true}); // headless to prevent xml declaration
-    svg.dtd (); // doctype svg
+    // build the skill map SVG
+    var svg = skillmapsvg.skillmap.svgbuilder.buildSkillMap (skills);
 
     // return the SVG document
-    res.send (svg.end ({pretty: true}));
+    res.send (svg);
 
 });
 
---
+++ svgbuilder.js
@@ -0,0 +1,211 @@
+/***************************************************************************
+ *
+ *  SVG Builder
+ *  Creates SVG documents, transforming data into visual elements.
+ *
+ ***************************************************************************/
+var xmlbuilder = require ("xmlbuilder");
+
+if (! exports.skillmap) exports.skillmap = {};
+
+exports.skillmap.svgbuilder = {
+
+    // specify the units for positioning information - only in case need to change for target application
+    units: {
+        layout: "in",
+        text: "pt"
+    },
+
+    // specify the position information for x/y coordinates of elements
+    pos: {
+        elementTopStart: 0.35,              // top position of first graph
+        elementTopSpacing: 0.5,            // spacing between top positions of graphs
+        labelLeftOffset: 1.5,               // right border position of text labels to left of graphs
+        labelTopOffset: 0.15,               // distance below the top of the graph where the bottom of the text displays
+        graphLeftOffset: 1.75               // left border position of the graphs
+    },
+
+    // specify the size information for width/height of elements
+    size: {
+        canvasMultiplier: 2,                // increases size of all elements which creates a larger canvas in the HTML
+        defaultPageHeight: 1,               // used when requesting blank SVG
+        fontLabel: 10,                      // size of the label to the left of the graph
+        fontMilestone: 6,                   // size of the milestone text under the graph
+        graphHeight: 0.15,                  // height of the graph
+        graphLineHeight: 0.01,              // height of the line under the graph
+        milestoneLineHeight: 0.075,         // line height for the milestone text
+        milestoneMarker: 0.04,              // the radius of the circle marking a milestone
+        pageWidth: 7.5                      // the width the image
+    },
+
+    // specify the colors
+    colors: {
+        graph: "#F00",                      // color inside the graphs
+        line: "#000",                       // color of the bottom border lines and milestone markers
+        textLabel: "#000",                  // color of the labels to the left of the graphs
+        textMilestone: "#555"               // color of the milestone text
+    },
+
+    // builds the full skill map SVG based on the skill data provided
+    buildSkillMap: function (skills) {
+
+        var linearGradients = [];
+        var stops = [];
+        var elements = [];
+
+        // add each skill
+        var numSkills = skills.length;
+        for (var i = 0; i < skills.length; i++) {
+
+            var skill = skills[i];
+
+            stops.push ({ stop: {
+                "@offset": "0%",
+                "@stop-color": this.colors.graph,
+                "@stop-opacity": "0"
+            }});
+            stops.push ({ stop: {
+                "@offset": "33.3%",
+                "@stop-color": this.colors.graph,
+                "@stop-opacity": "0.8"
+            }});
+            stops.push ({ stop: {
+                "@offset": "100%",
+                "@stop-color": this.colors.graph,
+                "@stop-opacity": "0.2"
+            }});
+
+            linearGradients.push ({ linearGradient: {
+                "@id": "gradient-1",
+                "#list": stops
+            }});
+
+            // add the label
+            elements.push ({ text: {
+                "@x": this.getPositionUnits (this.pos.labelLeftOffset),
+                "@y": this.getPositionUnits (this.pos.elementTopStart + this.pos.labelTopOffset + this.pos.elementTopSpacing * i),
+                "@class": "label",
+                "#text": skill.title
+            }});
+
+            // add the graph gradient
+            elements.push ({ rect: {
+                "@x": this.getPositionUnits (this.pos.graphLeftOffset),
+                "@y": this.getPositionUnits (this.pos.elementTopStart + this.pos.elementTopSpacing * i),
+                "@width": this.getPositionUnits (this.size.pageWidth - this.pos.graphLeftOffset),
+                "@height": this.getPositionUnits (this.size.graphHeight),
+                "@fill": "url(#gradient-1)"
+            }});
+
+            // add the line under the gradient
+            var lineTopPosition = this.pos.elementTopStart + this.size.graphHeight + this.pos.elementTopSpacing * i;
+            elements.push ({ line: {
+                "@x1": this.getPositionUnits (this.pos.graphLeftOffset),
+                "@y1": this.getPositionUnits (lineTopPosition),
+                "@x2": this.getPositionUnits (this.size.pageWidth),
+                "@y2": this.getPositionUnits (lineTopPosition),
+                "@class": "databox"
+            }});
+
+            // add a milestone marker
+            var milestoneOffset = 1.75; // temporary until adding real milestones
+            elements.push ({ circle: {
+                "@cx": this.getPositionUnits (this.pos.graphLeftOffset + milestoneOffset),
+                "@cy": this.getPositionUnits (lineTopPosition),
+                "@r": this.getPositionUnits (this.size.milestoneMarker),
+                "@fill": this.colors.line
+            }});
+
+            // add milestone text
+            elements.push ({ text: {
+                "#list": [
+                    { tspan: {
+                        "@x": this.getPositionUnits (this.pos.graphLeftOffset + milestoneOffset),
+                        "@y": this.getPositionUnits (lineTopPosition + this.size.milestoneMarker + this.size.milestoneLineHeight),
+                        "@class": "milestone",
+                        "#text": "This is a"
+                    }},
+                    { tspan: {
+                        "@x": this.getPositionUnits (this.pos.graphLeftOffset + milestoneOffset),
+                        "@y": this.getPositionUnits (lineTopPosition + this.size.milestoneMarker + this.size.milestoneLineHeight * 2),
+                        "@class": "milestone",
+                        "#text": "sample milestone"
+                    }}]
+            }});
+
+        }
+
+        // generate the CSS
+        var styleData = "\
+            text.label {\
+                text-anchor: end;\
+                font-size: " + this.getTextUnits (this.size.fontLabel) + ";\
+                fill: " + this.colors.textLabel + ";\
+            }\
+            tspan.milestone {\
+                text-anchor: middle;\
+                font-size: " + this.getTextUnits (this.size.fontMilestone) + ";\
+                fill: " + this.colors.textMilestone + ";\
+            }\
+            line.databox {\
+                stroke: " + this.colors.line + ";\
+                stroke-width: " + this.getPositionUnits (this.size.graphLineHeight) + ";\
+            }\
+        ";
+
+        // build the structure
+        var svgData = {};
+        if (elements.length) {
+            svgData = {
+                svg: {
+                    "@width": this.getPositionUnits (this.size.pageWidth),
+                    "@height": this.getPositionUnits (this.pos.elementTopSpacing * skills.length + this.pos.elementTopStart),
+                    "@version": "1.1",
+                    "@xmlns": "http://www.w3.org/2000/svg",
+                    defs: {
+                        style: {
+                            "@type": "text/css",
+                            "#cdata": styleData
+                        },
+                        "#list": linearGradients
+                    },
+                    "#list": elements
+                }
+            };
+        }
+
+        // if no elements are available, build an empty SVG
+        else {
+            svgData = {
+                svg: {
+                    "@width": this.getPositionUnits (this.size.pageWidth),
+                    "@height": this.getPositionUnits (this.size.defaultPageHeight),
+                    "@version": "1.1",
+                    "@xmlns": "http://www.w3.org/2000/svg"
+                }
+            }
+        }
+
+        // create the SVG document
+        var svg = xmlbuilder.create (svgData, {}, {}, {headless: true}); // headless to prevent xml declaration
+        svg.dtd (); // doctype svg
+
+        return svg.end ({pretty: true});
+
+    },
+
+    // returns the geometry data with the supplied unit
+    getPositionUnits: function (value) {
+
+        return value * this.size.canvasMultiplier + this.units.layout;
+
+    },
+
+    // returns the font size with the supplied unit
+    getTextUnits: function (value) {
+
+        return value * this.size.canvasMultiplier + this.units.text;
+
+    }
+
+}
