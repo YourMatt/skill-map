0296b16a5a838c3b0cad46bf2e158a1c2bf51f9a
Matt Sadowski <matt@ubuntu-2011.(none)>
2014-08-11 21:14:01 -0600

    Adding latest updates missed with the initial push.

--- app.js
+++ app.js
@@ -1,7 +1,188 @@
 
-var http = require ("http");
-var serv = http.createServer (function (req, res) {
-    res.writeHead (200, { 'Content-Type': 'text/html' });
-    res.end ('<h1>So far, so good...</h1>');
+var express = require ("express"),
+    util = require ("util"),
+    xmlbuilder = require ("xmlbuilder");
+
+var app = express ();
+app.use (express.static (__dirname + "/public"));
+
+// load raw post data to req rawBody param
+app.use(function(req, res, next) {
+
+    req.rawBody = "";
+
+    req
+        .on ("data", function(chunk) {
+            req.rawBody += chunk;
+        })
+        .on ("end", function() {
+            next();
+        });
+
 });
-serv.listen (80);
+
+// handle SVG requests
+app.route ("/*.svg").all (function (req, res) {
+
+    var instructions = req.rawBody || req.query.instructions || ""; // pulls from raw post, then query string if none provided
+
+    res.type ("svg");
+    console.log ("Requested SVG Image - Instructions: " + instructions);
+    var data = [];
+    try {
+        data = JSON.parse (instructions);
+    }
+    catch (err) {}
+
+    var linearGradients = [];
+    var stops = [];
+    var elements = [];
+
+    // add each skill
+    if (data.length) {
+
+        stops.push ({ stop: {
+            "@offset": "0%",
+            "@stop-color": "#F00",
+            "@stop-opacity": "0"
+        }});
+        stops.push ({ stop: {
+            "@offset": "33.3%",
+            "@stop-color": "#F00",
+            "@stop-opacity": "0.8"
+        }});
+        stops.push ({ stop: {
+            "@offset": "100%",
+            "@stop-color": "#F00",
+            "@stop-opacity": "0.2"
+        }});
+
+        linearGradients.push ({ linearGradient: {
+            "@id": "gradient-1",
+            "#list": stops
+        }});
+
+        // LOOP THROUGH SKILLS HERE
+
+        // add the label
+        elements.push ({ text: {
+            "@x": "1.5in",
+            "@y": "0.5in",
+            "@class": "label",
+            "#text": "Label"
+        }});
+
+        // add the graph gradient
+        elements.push ({ rect: {
+            "@x": "1.75in",
+            "@y": "0.35in",
+            "@width": "10in",
+            "@height": "0.15in",
+            "@fill": "url(#gradient-1)"
+        }});
+
+        // add the line under the gradient
+        elements.push ({ line: {
+            "@x1": "1.75in",
+            "@y1": "0.5in",
+            "@x2": "11.75in",
+            "@y2": "0.5in",
+            "@class": "databox"
+        }});
+
+        // add a milestone marker
+        elements.push ({ circle: {
+            "@cx": "3in",
+            "@cy": "0.5in",
+            "@r": "0.04in",
+            "@fill": "black"
+        }});
+
+        // add milestone text
+        elements.push ({ text: {
+            "#list": [
+                { tspan: {
+                    "@x": "3in",
+                    "@y": "0.65in",
+                    "@class": "milestone",
+                    "#text": "This is a"
+                }},
+                { tspan: {
+                    "@x": "3in",
+                    "@y": "0.75in",
+                    "@class": "milestone",
+                    "#text": "sample milestone"
+                }}]
+        }});
+
+    }
+
+    var styleData = "\
+        text.label {\
+            text-anchor: end;\
+            font-size: 12pt;\
+        }\
+        tspan.milestone {\
+            text-anchor: middle;\
+            font-size: 8pt;\
+        }\
+        line.databox {\
+            stroke: black;\
+            stroke-width: 0.01in;\
+        }\
+    ";
+
+    // build the structure
+    var svgData = {};
+    if (elements.length) {
+        svgData = {
+            svg: {
+                "@width": "12in",
+                "@height": "3in",
+                "@version": "1.1",
+                "@xmlns": "http://www.w3.org/2000/svg",
+                defs: {
+                    style: {
+                        "@type": "text/css",
+                        "#cdata": styleData
+                    },
+                    "#list": linearGradients
+                },
+                "#list": elements
+            }
+        };
+    }
+
+    // if no elements are available, build an empty SVG
+    else {
+        svgData = {
+            svg: {
+                "@width": "12in",
+                "@height": "1in",
+                "@version": "1.1",
+                "@xmlns": "http://www.w3.org/2000/svg"
+            }
+        }
+    }
+
+    // create the SVG document
+    var svg = xmlbuilder.create (svgData, {}, {}, {headless: true}); // headless to prevent xml declaration
+    svg.dtd (); // doctype svg
+
+    // return the SVG document
+    res.send (svg.end ({pretty: true}));
+
+});
+
+// handle all other requests
+app.set ("views", __dirname + "/views/layout");
+app.engine ("ejs", require ("ejs").renderFile);
+app.get ("/*", function (req, res) {
+
+    res.render ("template.ejs");
+
+});
+
+// start server
+app.listen (80);
+console.log ("Server started.");
--- public/js/skill-map.js
+++ public/js/skill-map.js
@@ -1,6 +1,87 @@
 
-$(document).ready(function () {
-
-    console.log("loaded");
-
+// data model for the skill data
+var Skill = (function () {
+    this.title = "";
 });
+
+// set up the angular module
+var app = angular.module ("skillMap", []);
+app.controller ("SkillMapController", ["$scope", function (scope) {
+
+    scope.skills = [];
+
+    // set up the available years
+    scope.years = getAvailableYears ();
+
+    // set up the skills
+    var skill = new Skill ();
+    scope.skills = [skill];
+
+    // checks if more input fields are needed and adds as necessary
+    scope.setInputFields = function (skipJsonReset) {
+
+        // check if any title is given for the last skill - if so, show another line for input
+        var lastSkill = $(scope.skills).get(-1);
+        if (lastSkill.title) {
+            scope.skills.push (new Skill ());
+        }
+
+        // reset the viewable JSON string
+        if (! skipJsonReset) {
+            scope.loadSkillsJson();
+        }
+
+    };
+
+    // updates the JSON structure from data provided in the input fields
+    scope.loadSkillsJson = function () {
+
+        // remove the last element if blank - not popping off the end because will affect existing skills without deep copy workaround
+        var newSkills = [];
+        var maxItem = scope.skills.length - 1;
+        for (var i = 0; i < maxItem; i++) {
+            newSkills.push (scope.skills[i]);
+        }
+
+        scope.skillsJson = angular.toJson (newSkills);
+
+    };
+
+    // updates the skills with data provided within the JSON viewer
+    scope.reloadSkillsFromJson = function () {
+
+        // load the skills from the JSON and publish for the controllers
+        try {
+            var newSkills = angular.fromJson (scope.skillsJson);
+            scope.skills = newSkills;
+            scope.setInputFields (true); // prevent circular loop
+        }
+        catch (err) { }
+
+    };
+
+    // rebuilds the image with the instructions provided by the current skill data
+    scope.regenerateSvgImage = function () {
+
+        $("#results-image").attr ("src", "skillmap.svg?instructions=" + scope.skillsJson);
+
+    };
+
+    // load the initial JSON string
+    scope.loadSkillsJson ();
+
+}]);
+
+function getAvailableYears () {
+
+    var yearStart = 2000;
+    var yearEnd = new Date ().getFullYear ();
+
+    var years = new Array ();
+    for (var i = yearStart; i <= yearEnd; i++) {
+        years.push (i);
+    }
+
+    return years;
+
+}
